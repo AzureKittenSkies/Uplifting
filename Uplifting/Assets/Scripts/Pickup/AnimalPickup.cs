beef148457f68daea4703b9fcee68ba74842c827
Unknown <azzhayman@gmail.com>
2018-11-14 12:01:34 +1100

    Particle systems, models and scripts
    Oh my!

---
+++ Uplifting/Assets/Scripts/Pickup/AnimalPickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Uplifting
+{
+    public class ButterflyPickup : MonoBehaviour
+    {
+        #region Particle System
+        public ParticleSystem particlesToPlayer;
+        public ParticleSystem particlesDisappear;
+        #endregion
+
+
+        public GameObject playerObject;
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerObject = other.gameObject;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    
+                }        
+            }
+        }
+
+        private void Collected()
+        {
+            particlesToPlayer.transform.LookAt(playerObject.transform, transform.up);
+            particlesToPlayer.Play();
+            particlesDisappear.Play();
+
+        }
+
+
+
+    }
+
+}
